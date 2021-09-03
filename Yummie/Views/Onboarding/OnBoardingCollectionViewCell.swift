d35882baaf6b88b3ee32862419f8d8827221c146
Victor Alvarenga <alvarengavictor73@gmail.com>
2021-09-03 14:33:42 +0100

    Added Comments

--- Yummie/Views/Onboarding/OnBoardingCollectionViewCell.swift
+++ Yummie/Views/Onboarding/OnBoardingCollectionViewCell.swift
@@ -14,7 +14,7 @@ class OnBoardingCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var slidedescriptionLabel: UILabel!
     
     func setup (_ slide:OnBoardingSlide){
-        
+        //set properties after beein called by ONBoardingViweController
         slideimageView.image = slide.image
         slidetitleLabel.text = slide.title
         slidedescriptionLabel.text = slide.description
--- Yummie/Views/Onboarding/OnBoardingViewController.swift
+++ Yummie/Views/Onboarding/OnBoardingViewController.swift
@@ -8,14 +8,20 @@
 import UIKit
 
 class OnBoardingViewController: UIViewController {
+    
+    // IB Outlets
     @IBOutlet weak var collectioView: UICollectionView!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var pageControl: UIPageControl!
     
     var slides : [OnBoardingSlide] = []
+    
+    //settin the page control property to the current page number via computed properties
     var currentPage = 0 {
         didSet{
+            
             pageControl.currentPage = currentPage
+            //change the button if the user is in the last page
             if currentPage == slides.count - 1{
                 nextButton.setTitle("Get Started", for: .normal)
             }else{
@@ -39,12 +45,14 @@ class OnBoardingViewController: UIViewController {
 
   
     @IBAction func nextButtonTapped(_ sender: UIButton) {
+        //Navigate to the home page if the user taps nexte after the last page
         if currentPage == slides.count - 1{
             let controller = storyboard?.instantiateViewController(identifier: "HomeVC") as! UINavigationController
             controller.modalPresentationStyle = .fullScreen
             controller.modalTransitionStyle = .flipHorizontal
             present(controller, animated: true, completion: nil)
         }else{
+            //if not, go to the next page
             currentPage += 1
             let indexPath = IndexPath(item: currentPage, section: 0)
             collectioView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
@@ -65,14 +73,17 @@ extension OnBoardingViewController : UICollectionViewDelegate, UICollectionViewD
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "OnBoardingCollectionViewCell", for: indexPath) as! OnBoardingCollectionViewCell
+        //send the properties to the cell class
         cell.setup(slides[indexPath.row])
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //set the sell size to fill the whole collection view
         return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
     }
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+       //detect if the user has finished scrolling to update page control indicator
         let width = scrollView.frame.width
         currentPage = Int(scrollView.contentOffset.x/width)
         
